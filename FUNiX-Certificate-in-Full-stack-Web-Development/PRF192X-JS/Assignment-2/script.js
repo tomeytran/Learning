4703f820be6cedcd76c4a26f3b679db76d0127cf
Tom <tomtran310251@gmail.com>
2023-09-10 22:56:35 +0700

    Update

--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-2/script.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-2/script.js
@@ -4,17 +4,3 @@ const sidebar = document.getElementById("sidebar");
 sidebar.onclick = function () {
   sidebar.classList.toggle("active");
 };
-const obj = {
-  firstName: "Tom",
-  lastName: "Tran",
-  age: 20,
-};
-function getThis() {
-  return this;
-}
-console.log(getThis()); // use strict = undefined, else = window
-
-const getThis2 = () => {
-  return this;
-};
-console.log(getThis2()); // use strict = window, else = window
