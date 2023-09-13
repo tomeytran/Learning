3e106519187e9f7efb89dd04cfb02fae31adb735
Tom <tomtran310251@gmail.com>
2023-09-13 18:49:26 +0700

    Update

--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-11/script.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-11/script.js
@@ -1,40 +1,49 @@
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    // Get answer
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
-      )
-    );
-    console.log(answer);
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // Get answer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
 
-    // Register answer
-    typeof answer === "number" &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//     // Register answer
+//     typeof answer === "number" &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults("string");
-  },
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
 
-  displayResults(type = "array") {
-    if (type === "array") {
-      console.log(this.answers);
-    } else if (type === "string") {
-      // Poll results are 13, 2, 4, 1
-      console.log(`Poll results are ${this.answers.join(", ")}`);
-    }
-  },
-};
+//   displayResults(type = "array") {
+//     if (type === "array") {
+//       console.log(this.answers);
+//     } else if (type === "string") {
+//       // Poll results are 13, 2, 4, 1
+//       console.log(`Poll results are ${this.answers.join(", ")}`);
+//     }
+//   },
+// };
 
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call({ answers: [5, 2, 3] }, "string");
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const newLi = document.createElement("li");
+const addNew = document.getElementById("addItem");
+const displayResult = result.append(newLi);
+addNew.addEventListener("click", displayResult);
+
+newLi.textContent = "Hello";
