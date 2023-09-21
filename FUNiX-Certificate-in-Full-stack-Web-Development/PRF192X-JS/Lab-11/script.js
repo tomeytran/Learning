0cdc94ba0d3ecf73b723009d079d873d20a08ced
Tom <tomtran310251@gmail.com>
2023-09-21 17:50:02 +0700

    update

--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-11/script.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-11/script.js
@@ -3,7 +3,7 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    // Get answer
+    // 1. Prompt alert
     const answer = Number(
       prompt(
         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
@@ -11,10 +11,16 @@ const poll = {
     );
     console.log(answer);
 
-    // Register answer
+    // 2. Register answer
+    if (isNaN(answer)) {
+      alert("Câu trả lời không hợp lệ");
+    } else if (answer >= this.answers.length) {
+      alert("Chỉ nhập 0-3");
+    }
+
     typeof answer === "number" &&
       answer < this.answers.length &&
-      this.answers[answer]++;
+      this.answers[answer]++; // if answer=number & =0-3 then increase the count (đoạn new Array(4)...là layout sẵn 4 slot, vd input 3 thì vị trí đó +1)
 
     this.displayResults();
     this.displayResults("string");
@@ -24,7 +30,7 @@ const poll = {
     if (type === "array") {
       console.log(this.answers);
     } else if (type === "string") {
-      // Poll results are 13, 2, 4, 1
+      // Poll results are a, b, c, d
       console.log(`Poll results are ${this.answers.join(", ")}`);
     }
   },
@@ -32,17 +38,8 @@ const poll = {
 
 document
   .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+  .addEventListener("click", poll.registerNewAnswer.bind(poll)); // phải thêm đoạn bind nếu ko nó chỉ đang trỏ
 
 poll.displayResults.call({ answers: [5, 2, 3] }, "string");
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
-
-// const input = document.getElementById("input");
-// const result = document.getElementById("result");
-// const newLi = document.createElement("li");
-// const addNew = document.getElementById("addItem");
-// const displayResult = result.append(newLi);
-// addNew.addEventListener("click", displayResult);
-
-// newLi.textContent = "Hello";
--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-12/script.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Lab-12/script.js
@@ -1,1 +1,1 @@
-console.log("Nice");
+"use strict";
