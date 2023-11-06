59d3f136b649f33191d5090c98011b0c300970ee
Tom <tomtran310251@gmail.com>
2023-11-06 09:43:58 +0700

    update

--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-3/scripts/register.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-3/scripts/register.js
@@ -1,2 +1,61 @@
 "use strict";
-const userArr = [];
+// const userArr = [];
+const firstName = document.getElementById("input-firstname");
+const lastName = document.getElementById("input-lastname");
+const userName = document.getElementById("input-username");
+const password = document.getElementById("input-password");
+const passwordConfirm = document.getElementById("input-password-confirm");
+const registerBtn = document.getElementById("btn-submit");
+
+registerBtn.addEventListener("click", function () {
+	const data = {
+		firstName: firstName.value,
+		lastName: lastName.value,
+		userName: userName.value,
+		password: password.value,
+		passwordConfirm: passwordConfirm.value,
+	};
+
+	const validate = validateData(data);
+
+	if (validate) {
+		userArr.push(data);
+		saveToStorage("userArr", userArr);
+		clearInput();
+		console.log(userArr);
+	}
+});
+
+function validateData(data) {
+	let isValidated = true;
+	if (data.firstName.trim() === "") {
+		alert("Please input first name");
+		isValidated = false;
+	}
+	if (data.lastName.trim() === "") {
+		alert("Please input last name");
+		isValidated = false;
+	}
+	if (data.userName.trim() === "") {
+		alert("Please input user name");
+		isValidated = false;
+	}
+	if (data.password.trim() === "") {
+		alert("Please input password");
+		isValidated = false;
+	} else if (data.passwordConfirm.trim() === "") {
+		alert("Please confirm password");
+		isValidated = false;
+	} else if (data.passwordConfirm !== data.password) {
+		alert("The password confirmation does not match");
+	}
+	return isValidated;
+}
+
+function clearInput() {
+	firstName.value = "";
+	lastName.value = "";
+	userName.value = "";
+	password.value = "";
+	passwordConfirm.value = "";
+}
--- FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-3/scripts/storage.js
+++ FUNiX-Certificate-in-Full-stack-Web-Development/PRF192X-JS/Assignment-3/scripts/storage.js
@@ -1,8 +1,27 @@
 "use strict";
+const user1 = {
+	firstName: "Johnny",
+	lastName: "Doe",
+	userName: "johnnyDoe",
+	password: "123456",
+};
+
+const user2 = {
+	firstName: "Tommy",
+	lastName: "Dan",
+	userName: "tommyDan",
+	password: "987654",
+};
+
+if (!getFromStorage("userArr")) {
+	saveToStorage("userArr", [user1, user2]);
+}
+const userArr = getFromStorage("userArr");
+
 // LOCAL STORAGE FUNCTIONS
 function saveToStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
+	localStorage.setItem(key, JSON.stringify(value));
 }
 function getFromStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+	return JSON.parse(localStorage.getItem(key));
 }
